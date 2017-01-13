e88a6b17d0ced9a0a16a4f59d948b6ea3e1344a3
Alexa Schlegel <alexa.schlegel@gmail.com>
2017-01-13 18:18:09 +0100

    changed parameters

--- bib/pipeline_cam.py
+++ bib/pipeline_cam.py
@@ -4,6 +4,7 @@ import numpy as np
 import networkx as nx
 import preprocessing as prep
 import sys
+import itertools
 
 from multiprocessing import Pool
 from bb_binary import FrameContainer, Repository, load_frame_container
@@ -15,22 +16,29 @@ def get_files(path, camid):
 	files = list(repo.iter_fnames(cam=camid))
 	return files
 
-def generate_network(index, file, confidence, distance, ilen):
+def generate_network(index, file, confidence, ilens, distances):
+
     
 	fc = load_frame_container(file)
 	df = prep.get_dataframe2(fc)
 	df = prep.calcIds(df,confidence)
-	close = prep.get_close_bees_ckd(df, distance)
-	p = prep.bee_pairs_to_timeseries(close)
 
-	return prep.extract_interactions(p,ilen)
+	result = []
+
+	for distance in distances:
+		close = prep.get_close_bees_ckd(df, distance)
+		p = prep.bee_pairs_to_timeseries(close)
+
+		for ilen in ilens:
+			r = prep.extract_interactions(p,ilen)
+			restult.append((ilen,distance,r))
+
+	return result
 
 
-def run(p, conf=.95, dist=160, ilen=3, c=None, filename="template", camid=0):
+def run(p, conf=.95, c=None, filename="template", camid=0):
 	path = p
 	confidence = conf
-	distance = dist
-	interaction_len = ilen
 	cpus = c
 
 	# make file file packges
@@ -43,42 +51,49 @@ def run(p, conf=.95, dist=160, ilen=3, c=None, filename="template", camid=0):
 
 	pool = multiprocessing.Pool(cpus)
 
+	ilens = [3,6,9,12,15,18,21] #ilen
+	distances = [100,110,120,130,140,150,160] #distance
+	l = list(itertools.product(ilens,distances))
+
 	tasks = []
 
 	for e, f in enumerate(files):
-		tasks.append((e, f, confidence, distance, interaction_len))
+		tasks.append((e, f, confidence, ilens, distances))
 
 	results = [pool.apply_async( generate_network, t ) for t in tasks]
 
 	# Egebnisse zusammenbasteln
-	edges = []
+	edges = {item:[] for item in l}
 
 	for result in results:
-		edges.append(result.get())
+		# ein result ist eine liste von ganz vielen (d,i,r)s
+		reslist = result.get()
 
-	G = prep.create_graph2(pd.concat(edges))
-	nx.write_graphml(G, "{}_{}conf_{}dist_{}ilen_cam{}.graphml".format(
-		filename,
-		str(confidence),
-		str(distance),
-		str(interaction_len),
-		str(camid)))
+		for i,d,r in reslist:
+			edges[(i,d)].extend(r)
 
-	print(nx.info(G))
+	for i,d in edges:
+		G = prep.create_graph2(pd.concat(edges[(i,d)]))
+		nx.write_graphml(G, "{}_{}conf_{}dist_{}ilen_cam{}.graphml".format(
+			filename,
+			str(confidence),
+			str(d),
+			str(i),
+			str(camid)))
+
+		print(nx.info(G))
 
 
 if __name__ == '__main__':
 
-	if (len(sys.argv) == 8 ):
+	if (len(sys.argv) == 6 ):
 		path = sys.argv[1]		
 		conf = float(sys.argv[2])
-		dist = int(sys.argv[3])
-		ilen = int(sys.argv[4])
-		c = int(sys.argv[5])
-		f = str(sys.argv[6])
-		camid = int(sys.argv[7])
+		c = int(sys.argv[3])
+		f = str(sys.argv[4])
+		camid = int(sys.argv[5])
 
-		run(path, conf, dist, ilen, c, f, camid)
+		run(path, conf, c, f, camid)
 	else:
-		print("Usage:\npython3 pipeline.py <path> <confidence> <radius> <interaction length> <number of processes> <filename> <camid>")
-		print("Example:\npython3 pipeline.py 'path/to/data' 0.95 160 3 16 myfilename 0")
+		print("Usage:\npython3 pipeline.py <path> <confidence> <number of processes> <filename> <camid>")
+		print("Example:\npython3 pipeline.py 'path/to/data' 0.95 16 myfilename 0")
