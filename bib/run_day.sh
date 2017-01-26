59456ecabe13c4932896aaa9fa1b60a85c49d34a
Alexa Schlegel <alexa.schlegel@googlemail.com>
2017-01-26 13:51:24 +0100

    Bash Script for multiple networks

--- bib/run_day.sh
+++ bib/run_day.sh
@@ -1,4 +1,18 @@
-python3 pipeline_cam.py /mnt/data/2015082216/ 0.95 8 "2015082216" 0
+python3 pipeline_frames_parallel.py /storage/mi/aschle/days/ 2015-08-21T00:00:00Z 1440 0.99 160 6 8 2015-08-21T00:00:00Z_1d_allCams
 
-python3 pipeline_cam.py /mnt/data/2015082215/ 0.95 8 "2015082215" 0
+python3 pipeline_frames_parallel.py /storage/mi/aschle/days/ 2015-08-22T00:00:00Z 1440 0.99 160 6 8 2015-08-22T00:00:00Z_1d_allCams
 
+python3 pipeline_frames_parallel.py /storage/mi/aschle/days/ 2015-08-23T00:00:00Z 1440 0.99 160 6 8 2015-08-23T00:00:00Z_1d_allCams
+
+python3 pipeline_frames_parallel.py /storage/mi/aschle/days/ 2015-08-24T00:00:00Z 1440 0.99 160 6 8 2015-08-24T00:00:00Z_1d_allCams
+
+python3 pipeline_frames_parallel.py /storage/mi/aschle/days/ 2015-08-25T00:00:00Z 1440 0.99 160 6 8 2015-08-25T00:00:00Z_1d_allCams
+
+#for i in "21"
+#"22" "23" "24" "25"
+
+#	do
+#	echo $i 
+#	python3 pipeline_frames_parallel.py /storage/mi/aschle/days/ 2015-08-$iT00:00:00Z 1440 0.99 160 6 8 2015-08-$iT00:00:00Z_1d_allCams
+
+#done
--- bib/run_hour.sh
+++ bib/run_hour.sh
@@ -1,8 +1,8 @@
 for i in "00" "01" "02" "03" "04" "05" "06" "07" "08" "09" "10" "11" "12" "13" "14" "15" "16" "17" "18" "19" "20" "21" "22" "23"
 
 	do
-	echo $i
-	python3 pipeline_frames_parallel.py /storage/mi/aschle/1hour/20150821-$i/ 0.99 160 6 12 20150821-$i-1h-allCams
+	echo $i 
+	python3 pipeline_frames_parallel.py /storage/mi/aschle/1day/ 2015-08-21T$i:00:00Z 60 0.99 160 6 12 2015-08-21T$i:00:00Z_1h_allCams
 
 done
 
