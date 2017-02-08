4e087a2258ffa048ba12afee61830038f985bb81
Alexa Schlegel <alexa.schlegel@googlemail.com>
2017-02-09 00:17:49 +0100

    added the yead 2015 or 2016 for Id calculation

--- bib/pipeline_frames_parallel.py
+++ bib/pipeline_frames_parallel.py
@@ -13,7 +13,7 @@ from collections import namedtuple
 from pandas import DataFrame, Series
 
 
-def generate_network(enu, path, b, e, confidence, distance, ilen):
+def generate_network(enu, path, b, e, confidence, distance, ilen, year):
     
     repo = Repository(path)
     xmax = 3000
@@ -39,7 +39,7 @@ def generate_network(enu, path, b, e, confidence, distance, ilen):
         
         df = DataFrame(tpls)
         print("#{} DF-{}: {}, {}, {}".format(enu, i, df.shape, datetime.datetime.fromtimestamp(b, tz=pytz.UTC),datetime.datetime.fromtimestamp(e, tz=pytz.UTC)))
-        df = prep.calcIds(df,confidence)
+        df = prep.calcIds(df,confidence, year)
         parts[i] = df
         
     # cam 0 und cam1 nach rechts verschieben
@@ -60,13 +60,14 @@ def generate_network(enu, path, b, e, confidence, distance, ilen):
     return prep.extract_interactions(p,ilen)
 
 
-def run(path, start_ts, network_size, confidence=.95, distance=160, interaction_len=3, numCPUs=None, filename="template"):
+def run(path, start_ts, network_size, confidence=.95, distance=160, interaction_len=3, numCPUs=None, filename="template", year=2015):
 
     p = path
     c = confidence
     dist = distance
     ilen = interaction_len
     cpus = numCPUs
+    y = year
 
     pool = multiprocessing.Pool(cpus)
 
@@ -94,7 +95,7 @@ def run(path, start_ts, network_size, confidence=.95, distance=160, interaction_
     for enu, i in enumerate(list(range(parts))):
         b = begin_ts + (i * slice_len)
         e = (b-0.000001) + (slice_len)
-        tasks.append((enu, p, b, e, c, dist, ilen))
+        tasks.append((enu, p, b, e, c, dist, ilen, y))
 
     results = [pool.apply_async( generate_network, t ) for t in tasks]
 
@@ -114,7 +115,7 @@ def run(path, start_ts, network_size, confidence=.95, distance=160, interaction_
 
 if __name__ == '__main__':
 
-    if (len(sys.argv) == 9):
+    if (len(sys.argv) == 10):
         path = sys.argv[1]
 
         start = sys.argv[2]
@@ -128,9 +129,10 @@ if __name__ == '__main__':
         ilen = int(sys.argv[6])
         c = int(sys.argv[7])
         f = str(sys.argv[8])
+        year = int(sys.argv[9])
 
-        run(path, start_ts, size, conf, dist, ilen, c, f)
+        run(path, start_ts, size, conf, dist, ilen, c, f, year)
 
     else:
-        print("Usage:\npython3 pipeline_frames_parallel.py <path> <start-date as yyyy-mm-ddThh:mm:ssZ> <network_size in minutes> <confidence> <radius> <interaction length> <number of processes> <filename>")
-        print("Example:\npython3 pipeline.py 'path/to/data' 2015-08-21T00:00:00Z 60 0.95 160 3 16 myfilename")
+        print("Usage:\npython3 pipeline_frames_parallel.py <path> <start-date as yyyy-mm-ddThh:mm:ssZ> <network_size in minutes> <confidence> <radius> <interaction length> <number of processes> <filename> <year>")
+        print("Example:\npython3 pipeline.py 'path/to/data' 2015-08-21T00:00:00Z 60 0.95 160 3 16 myfilename 2015")
